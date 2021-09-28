9a3a270a299e5f3cae39d3a0261dec726708a735
wisawachoti <guypeach@hotmail.co.th>
2021-09-29 00:58:16 +0700

    new page

---
+++ wed/index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ใบเสนอราคา</title>
+</head>
+<body>
+    <h1>ใบเสนอราคา</h1>
+    <a href="page2.php">รายละเอียดใบเสนอราคา</a>
+    <a href="page3.php">ช่วงราคาของสินค้า</a>
+</body>
+</html>
