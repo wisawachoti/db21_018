c11dd331dc5dda56b20cafb387004b9ebeef8dcf
Potjarinn <potjarin.p@ku.th>
2021-10-01 02:52:12 +0700

    Update priceModels.php

--- mywed/models/priceModels.php
+++ mywed/models/priceModels.php
@@ -5,17 +5,19 @@ class priceModels{
     public $price;
     public $detail;
     public $quantity;
+    public $qtyp_id;
 
-    public function __construct($product_id,$price_color,$price,$detail,$quantity)
+    public function __construct($product_id,$price_color,$price,$detail,$quantity,$qtyp_id)
     {
         $this->product_id = $product_id;
         $this->price_color = $price_color;
         $this->price = $price;
         $this->detail = $detail;
         $this->quantity = $quantity;
+        $this->qtyp_id = $qtyp_id;
     }
 
-    public static function get(product_id)
+    public static function get($product_id)
     {
         require("connection_connect.php");
         $sql = "SELECT * FROM Quantity WHERE product_id = $product_id";
@@ -26,16 +28,17 @@ class priceModels{
         $price = $my_row[price];
         $detail = $my_row[detail];
         $quantity = $my_row[qauantity];
+        $qtyp_id = $my_row[qtyp_id];
         require("connection_close.php");
 
-        return new pricemodels($product_id,$price_color,$price,$detail,$quantity);
+        return new priceModels($product_id,$price_color,$price,$detail,$quantity,$qtyp_id);
     }
 
     public static function getAll()
     {
         $pricemodelsList = [];
         require("connection_connect.php");
-        $sql = "SELECT * FROM Quantity";
+        $sql = "SELECT product_id,price_color,price,detail,quantity FROM Quantity";
         $result = $conn->query($sql);
         while($my_row = $result->fetch_assoc())
         {
@@ -44,7 +47,8 @@ class priceModels{
             $price = $my_row[price];
             $detail = $my_row[detail];
             $quantity = $my_row[quantity];
-            $pricemodelsList[] = new pricemodels($product_id,$price_color,$price,$detail,$quantity);
+            $qtyp_id = $my_row[qtyp_id];
+            $pricemodelsList[] = new priceModels($product_id,$price_color,$price,$detail,$quantity,$qtyp_id);
         }
         require("connection_close.php");
 
@@ -55,7 +59,7 @@ class priceModels{
     {
         require("connection_connect.php");
         $sql = "SELECT * FROM Quantity WHERE (product_id like '%$key%' or price_color like '%$key%' or 
-        price like '%$key%' or detail like '%$key%' or quantity like '%$key%')";
+        price like '%$key%' or detail like '%$key%' or quantity like '%$key%' or qtyp_id like '%$qtyp_id%')";
         $result = $conn->query($sql);
         while($my_row = $result->fetch_assoc())
         {
@@ -64,27 +68,29 @@ class priceModels{
             $price = $my_row[price];
             $detail = $my_row[detail];
             $quantity = $my_row[quantity];
-            $pricemodelsList[] = new pricemodels($product_id,$price_color,$price,$detail,$quantity);
+            $qtyp_id = $my_row[qtyp_id];
+            $pricemodelsList[] = new priceModels($product_id,$price_color,$price,$detail,$quantity,$qtyp_id);
         }
         require("connection_close.php");
 
         return $pricemodelsList;
     }
 
-    public static function update($product_id,$price_color,$price,$detail,$quantity)
+    public static function add($product_id,$price_color,$price,$detail,$quantity,$qtyp_id)
     {
         require("connection_connect.php");
-        $sql = "INSERT INTO Quantity(product_id,price_color,price,detail,quaantity) values 
-        ('product_id','price_color','price','detail','quantity')";
+        $sql = "INSERT INTO Quantity(product_id,price_color,price,detail,quantity,qtyp_id) values 
+        ('product_id','price_color','price','detail','quantity','qtyp_id')";
         $result = $conn->query($sql);
         require("connect_cloes.php");
         return "add success $result rows";
     }
 
-    public static function update($product_id,$price_color,$price,$detail,$quantity)
+    public static function update($product_id,$price_color,$price,$detail,$quantity,$qtyp_id)
     {
         require("connection_connect.php");
-        $sql = "UPDATE Quantity SET product_id = '$product_id',price_color = '$price_color',price = '$price',detail = '$detail',quantity = '$quantity'";
+        $sql = "UPDATE Quantity SET product_id = '$product_id',price_color = '$price_color',
+        price = '$price',detail = '$detail',quantity = '$quantity',qtyp_id = '$qtyp_id'";
         $result = $conn->query($sql);
         require("coonnection_close.php");
         return "update success $result row";
