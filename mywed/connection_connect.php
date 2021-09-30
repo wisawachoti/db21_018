303427e4587789ec8fa97d694a32b7404a2a3877
Potjarinn <potjarin.p@ku.th>
2021-10-01 02:51:41 +0700

    Update connection_connect.php

--- mywed/connection_connect.php
+++ mywed/connection_connect.php
@@ -3,11 +3,11 @@ $servername = "localhost";
 $username = "db21_018";
 $password = "db21_018";
 
-$dbname = "";
+$dbname = "db21_018";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password);
-
+mysqli_set_charset($conn, "utf8");
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
@@ -16,5 +16,5 @@ if (!$conn) {
 if(!$conn->select_db($dbname)){
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
 ?>
