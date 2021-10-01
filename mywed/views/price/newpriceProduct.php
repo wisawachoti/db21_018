c1f711868275218d1975099816f81a274800a7b2
Potjarinn <potjarin.p@ku.th>
2021-10-02 04:23:50 +0700

    Update newpriceProduct.php

--- mywed/views/price/newpriceProduct.php
+++ mywed/views/price/newpriceProduct.php
@@ -1,13 +1,13 @@
 <fome method="get" action="">
-    <label>product_id<select name ="price_id">
+    <br>
     <label>price_color<input type="text" name="price_color"/> </label><br>
     <label>price<input type="text" name="price"/> </label><br>
     <label>detail<input type="text" name="detail"/> </label><br>
-    <label>quantity<input type="text" name="quantity">
+    <label>quantity<input type="text" name="quantity"/> </label><br>
+    <label>product_id<select name ="price_id">
 <?php foreach($pricemodelsList as $priceProduct)
 {
-    echo "option value=$priceProduct->product_id>
-    $priceProduct->pname</option>";
+    echo "<option value=$priceProduct->product_id > $priceProduct->pname</option>";
 }?>
 </select></label><br>
 <input type="hidden" name="controller" value="priceproduct"/>
