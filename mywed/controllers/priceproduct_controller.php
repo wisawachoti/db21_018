ac207bc81693a56bd76337720e53dd3c78ea33e6
Potjarinn <potjarin.p@ku.th>
2021-10-02 00:59:29 +0700

    Update priceproduct_controller.php

--- mywed/controllers/priceproduct_controller.php
+++ mywed/controllers/priceproduct_controller.php
@@ -6,4 +6,29 @@ class priceproduct_controller
         $pricemodelsList = priceModels::getAll();
         require_once('views/price/indexprice.php');
     }
+
+    public function newpriceProduct()
+    {
+        $pricemodelsList = priceModels::getAll();
+        require_once('views/price/indexprice.php');
+    }
+
+    public function addProduct()
+    {
+        $product_id = $_GET['product_id'];
+        $price_color = $_GET['price_color'];
+        $price = $_GET['price'];
+        $detail = $_GET['detail'];
+        $quantity = $_GET['quantity'];
+        priceModels::Add($product_id,$price_color,$price,$detail,$quantity);
+
+        priceproduct_controller::index();
+     }
+
+     public function search()
+     {
+         $key = $_GET['key'];
+         $pricemodelsList = priceModels::search($key);
+         require_once('views/price/indexprice.php');
+     }
 }?>
