73b7a11f4b0744534f63ef94ba68d43242e42a23
Potjarinn <potjarin.p@ku.th>
2021-10-02 01:01:19 +0700

    Create newpriceProduct.php

---
+++ mywed/views/price/newpriceProduct.php
@@ -0,0 +1,16 @@
+<fome method="get" action="">
+    <label>product_id<select name ="price_id">
+    <label>price_color<input type="text" name="price_color"/> </label><br>
+    <label>price<input type="text" name="price"/> </label><br>
+    <label>detail<input type="text" name="detail"/> </label><br>
+    <label>quantity<input type="text" name="quantity">
+<?php foreach($pricemodelsList as $priceProduct)
+{
+    echo "option value=$priceProduct->product_id>
+    $priceProduct->pname</option>";
+}?>
+</select></label><br>
+<input type="hidden" name="controller" value="priceproduct"/>
+<button type="submit" name="action" value="index">Back</button>
+<button type="submit" name="action" value="addProduct">Save</button>
+</form>
