e4dc9db4bddb5ff576fcdbd15f2e3854d1fbfe77
Potjarinn <potjarin.p@ku.th>
2021-10-02 01:58:03 +0700

    Update priceModels.php

--- mywed/models/priceModels.php
+++ mywed/models/priceModels.php
@@ -60,8 +60,8 @@ class priceModels{
     {
         require("connection_connect.php");
         $sql = "SELECT Quantity.product_id,pname,price_color,price,Quantity.detail,quantity FROM Quantity INNER JOIN Product 
-        ON Quantity.product_id = Product.product_id WHERE (Quantity.product_id like '%$key%' or Product.pname like '%$pname%' or Quantity.price_color like '%$key%' or 
-        Quantiity.price like '%$key%' or Quantity.detail like '%$key%' or Quantity.quantity like '%$key%')";
+        ON Quantity.product_id = Product.product_id WHERE (Quantity.product_id like '%$key%' or Product.product_id like '%$key%' or Product.pname like '%$pname%' or Quantity.price_color like '%$key%' or 
+        Quantity.price like '%$key%' or Quantity.detail like '%$key%' or Product.detail like '%$key%' or Quantity.quantity like '%$key%') AND Quantity.product_id = Product.product_id";
         $result = $conn->query($sql);
         while($my_row = $result->fetch_assoc())
         {
