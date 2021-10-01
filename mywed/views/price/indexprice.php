703e15f4c80da59c218449456e629c454b853196
Potjarinn <potjarin.p@ku.th>
2021-10-01 22:37:06 +0700

    Update indexprice.php

--- mywed/views/price/indexprice.php
+++ mywed/views/price/indexprice.php
@@ -1,5 +1,6 @@
 <table border = 3>
 <tr> <td>product_id</td>
+<td>pname</td>
 <td>price_color</td>
 <td>price</td>
 <td>detail</td>
@@ -8,6 +9,7 @@
 <?php foreach($pricemodelsList as $priceproduct)
 {
     echo "<tr> <td>$priceproduct->product_id</td>
+    <td>$priceproduct->pname</td>
     <td>$priceproduct->price_color</td> <td>$priceproduct->price</td> 
     <td>$priceproduct->detail</td> <td>$priceproduct->quantity</td> 
     <td>udate</td> <td>delete</td> </tr>";
