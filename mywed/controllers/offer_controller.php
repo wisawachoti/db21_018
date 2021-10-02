894c3d86912b5f0e21ac924572d2bf889b6764df
CHUSANA <91516445+CHUSANA@users.noreply.github.com>
2021-10-03 03:23:22 +0700

    Update offer_controller.php

--- mywed/controllers/offer_controller.php
+++ mywed/controllers/offer_controller.php
@@ -1,7 +1,7 @@
 <?php
 class offerController{
     public function index(){
-        $offerr_list = OfferModel::getAll();
+        $offer_list = OfferModel::getAll();
         require_once('views/offer/index_offer.php');
     }
 
