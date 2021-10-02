f08af8ecacd34ea5b1b33d297ba5c335c376260e
Potjarinn <potjarin.p@ku.th>
2021-10-03 00:05:36 +0700

    Update productModels.php

--- mywed/models/productModels.php
+++ mywed/models/productModels.php
@@ -15,7 +15,6 @@ class productModels{
 
     public static function getAll()
     {
-        echo "hi";
         $productmodelsList = [];
         require("connection_connect.php");
         $sql = "SELECT * FROM Product";
