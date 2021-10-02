613d9becb8421051e6220926f7f9a72b44b635da
CHUSANA <91516445+CHUSANA@users.noreply.github.com>
2021-10-03 03:23:24 +0700

    Update OfferModel.php

--- mywed/models/OfferModel.php
+++ mywed/models/OfferModel.php
@@ -7,32 +7,32 @@
     public $customerID;
     public $employeeID;
 
-    public function_construct($id,$date,$payment,$pay_m,$customerID,$employeeID){
+    public function _construct ($id,$date,$payment,$pay_m,$customerID,$employeeID){
         $this->id = $id;
         $this->date = $date;
         $this->payment = $payment;
         $this->pay_m = $pay_m;
-        $this->CustomerID = $customerID;
+        $this->customerID = $customerID;
         $this->employeeID = $employeeID;
     }
 
     public static function getAll(){
-        $offerList = [];
+        $offer_list = [];
         require("connection_connect.php");
-        $sql = "select * from Offer";
-        $result = conn->query($sql);
+        $sql = "SELECT * FROM Offer";
+        $result = $conn->query($sql);
         while($my_row = $result->fetch_assoc()){
-            $id = $my_row[offer_id];
-            $date = $my_row[Date];
-            $payment = $my_row[payment];
-            $pay_m = $my_row[pay_m];
-            $customerID = $my_row[CustomerID];
-            $employeeID = $my_row[employee_id];
-            $offerList[] = new OfferModel($id,$date,$payment,$pay_m,$customerID,$employeeID);
+            $id = $my_row['offer_id'];
+            $date = $my_row['Date'];
+            $payment = $my_row['payment'];
+            $pay_m = $my_row['pay_m'];
+            $customerID = $my_row['CustomerID'];
+            $employeeID = $my_row['employee_id'];
+            $offer_list[] = new OfferModel($id,$date,$payment,$pay_m,$customerID,$employeeID);
         }
         require("connection_close.php");
 
-        return $offerList;
+        return $offer_list;
     }
  }
 ?>
