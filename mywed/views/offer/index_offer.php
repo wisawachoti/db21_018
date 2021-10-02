70f05a2e9ac08d742aebe3c5548c5a956a8c95a3
CHUSANA <91516445+CHUSANA@users.noreply.github.com>
2021-10-03 05:48:47 +0700

    Update index_offer.php

--- mywed/views/offer/index_offer.php
+++ mywed/views/offer/index_offer.php
@@ -11,19 +11,18 @@
      <td>update</td>
      <td>delete</td> 
 </tr>
- 
  <?php foreach($offer_list as $Offer){
     echo "<tr> 
                  <td>$Offer->id</td> 
-                 <td>$Offer->date</td>
+                 <td>$Offer->date</td> 
                  <td>$Offer->payment</td> 
-                 <td>$Offer->pay_m</td>
-                 <td>$Offer->customerID</td> 
-                 <td>$Offer->employeeID</td>
+                 <td>$Offer->pay_m</td> 
+                 <td>$Offer->customerID</td>
+                 <td>$Offer->employeeID</td>  
                  <td>update</td> 
                  <td>delete</td> 
          </tr>";
-    }
+ }
     echo "</table>";
  
 ?>
