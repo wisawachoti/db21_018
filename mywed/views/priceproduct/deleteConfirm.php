3e5c84a4a524a01b66fc59fb1e770361b04a3570
Potjarinn <potjarin.p@ku.th>
2021-10-03 22:53:41 +0700

    Update deleteConfirm.php

--- mywed/views/priceproduct/deleteConfirm.php
+++ mywed/views/priceproduct/deleteConfirm.php
@@ -1,6 +1,6 @@
 <?php echo "<br>Are you sure to delete this student <br>
-            <br>  $priceproduct->product_id  $priceproduct->price_color  $priceproduct->price 
-            $priceproduct->detail  $priceproduct->quantity  $priceproduct->qtyp_id<br>";?>
+            <br>  product_id:$priceproduct->product_id  <br>price_color:$priceproduct->price_color  <br>price:$priceproduct->price 
+            <br>detail:$priceproduct->detail  <br>quantity:$priceproduct->quantity  <br>qtyp_id:$priceproduct->qtyp_id<br>";?>
 <form method="get" action="">
     <input type="hidden" name="controller" value="priceproduct"/>
     <input type="hidden" name="qtyp" value="<?php echo $priceproduct->qtyp_id;?>"/>
