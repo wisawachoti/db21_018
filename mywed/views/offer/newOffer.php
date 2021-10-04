c2311d2cf3d1b06a5da1fdd110523b962cda5530
CHUSANA <91516445+CHUSANA@users.noreply.github.com>
2021-10-04 22:29:40 +0700

    Update newOffer.php

--- mywed/views/offer/newOffer.php
+++ mywed/views/offer/newOffer.php
@@ -2,8 +2,9 @@
 <label> offerID <input type="text" name ="offerid"/> </label><br> 
 <label> Date <input type="text" name ="date"/> </label><br> 
 <label> Payment <input type="text" name ="payment"/> </label><br> 
+<label> pay_m <input type="text" name ="paym"/> </label><br>
 
-<label> EmployeeID 
+<label> EmployeeName 
     <select name="emID"> 
      <?php 
         foreach($employee_list as $em){
@@ -12,8 +13,17 @@
      ?>
     </select>
 </label><br>
+<label> CustomerName
+    <select name="cusID"> 
+     <?php 
+        foreach($customer_list as $cus){
+            echo "<option value = $cus->customer_id> $cus->name</option>";
+        }
+     ?>
+    </select>
+</label><br>
     
-    <input type="hidden" name="controller" value="Offer"/>
+    <input type="hidden" name="controller" value="offer"/>
     <button type="submit" name="action" value="index">Back</button>
-    <button type="submit" name="action" value="addProduct">Save</button>
+    <button type="submit" name="action" value="addOffer">Save</button>
 </form>
