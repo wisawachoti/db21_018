e3128623ed55a07b078ed837f7c5267c794529d7
wisawachoti <guypeach@hotmail.co.th>
2021-10-04 23:10:45 +0700

    nice day good bye good luck

--- mywed/views/detaildb/index.php
+++ mywed/views/detaildb/index.php
@@ -1,14 +1,97 @@
-<p>วิศวะ โชติพันธุ์พงศ์</p>
+<h1>รายละเอียดใบเสนอราคา</h1>
+<h2>วิศวะ โชติพันธุ์พงศ์ 6220500725</h2>
 
+<style>
+    .content-table {
+        border-collapse: collapse;
+        margin: auto;
+        font-size: 0.9em;
+        min-width: 500px;
+        border-radius: 12px 12px 12px 12px;
+        overflow: hidden;
+        /*box-shadow: 0 0 8px rgba(10, 50, 50, 5);*/
+    }
+
+    .content-table thead tr {
+        background-color: rgb(64, 30, 15);
+        border-bottom: 1px rgb(64, 30, 15);
+        border-left: 1px rgb(64, 30, 15);
+        border-right: 1px rgb(64, 30, 15);
+        color: #ffffff;
+        text-align: center;
+        font-weight: bold;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    .content-table th ,
+    .content-table td {
+        padding: 12px 15px;
+    }
+    .content-table tbody tr {
+        text-align: center;
+        color:#000000;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        border-bottom: 1px solid #e6dbd8;
+        border-left: 1px solid #e6dbd8;
+        border-right: 1px solid #e6dbd8;
+    }
+    .content-table tbody tr:nth-of-type(even) {
+        background-color: #e6dbd8;
+    }
+    .content-table tbody tr:nth-of-type(odd) {
+        background-color: #f0e7e4;
+    }
+    .content-table tbody tr:last-of-type {
+        border-bottom: 5px solid rgb(64, 30, 15);
+    }
+    
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    
+    input[type=text] {
+        width: 10%;
+        padding: 5px 8px;
+        margin: 5px 0px;
+        text-align: center;
+        box-sizing: border-box;
+        border: none;
+        background-color: rgb(64, 30, 15);
+        color: white;
+    }
+    .button {
+        padding: 5.75px 8px;
+        text-align: center;
+        text-decoration: none;
+        font-size: 12px;
+        margin: 0px 0px;
+        transition-duration: 0.4s;
+        cursor: pointer;
+    }
+
+    .button1 {
+        background-color: rgb(64, 30, 15); 
+        color: white; 
+        border: 2px solid rgb(64, 30, 15);
+    }
+
+    .button1:hover {
+        background-color: white;
+        color: rgb(64, 30, 15);
+        border: 2px solid white;
+    }
+
+</style>
+<html><body>
 <br>new priceProduct <a href=?controller=detaildb&action=newofferdetail>Click</a><br>
 
-<table border=1>
+
 <form method="get" action="">
     <input type="text" name="key">
     <input type="hidden" name="controller" value="detaildb"/>
     <button type="submit" name="action" value="search">
 Search</button>
-</form>
+</body></form></html>
+<table  class= "content-table" border = 3><thead>
 
     <tr>
        
@@ -19,6 +102,7 @@ Search</button>
         <td>จำนวนสี</td>
         <td>update</td><td>delete</td> 
     </tr>
+    </thead></tbody>
     <?php foreach ($offerdetailList as $offerdetail) {
         echo "<tr> 
 
@@ -33,3 +117,4 @@ Search</button>
     }
     echo "</table>";
     ?>
+     </tbody>
